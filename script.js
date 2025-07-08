b489c72038281dbc45418c12f9b852a88641c1ec
jamesl0350 <locklear.james.jl@gmail.com>
2025-07-08 05:22:44 -0400

    added button and form, fixed typos

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const myLibrary = [];
+const addedBookIds = [];
+const booksContainer = document.querySelector(".books-container")
+const bookForm = document.querySelector(".book-form")
+const addNewBookBtn = document.querySelector(".new-book-btn")
 
 function Book(title, author, pages, read, id) {
   this.title = title;
@@ -11,5 +15,17 @@ function Book(title, author, pages, read, id) {
 function addBookToLibrary(title, author, pages, read) {
   const id = crypto.randomUUID();
   const newBook = new Book(title, author, pages, read, id);
-  displayBook();
+  myLibrary.push(newBook);
+  displayBooks();
+}
+
+function displayBooks() {
+  myLibrary.forEach(book => {
+    if (!addedBookIds.includes(book.id)) {
+      const bookCard = createBookCard(book);
+      booksContainer.appendChild(bookCard)
+      addedBookIds.push(book.id)
+    }
+  });
+
 }
